2b0f1fe7ae8cfdf4137677166174fe05d460ad0d
Alex J. Champandard <445208+alexjc@users.noreply.github.com>
2020-02-01 21:57:19 +0100

    🛠️ Configuration of Sphinx as generator.

---
+++ src/conf.py
@@ -0,0 +1,13 @@
+# -- Project information -----------------------------------------------------
+project = 'The Science and Magic of Creativity'
+copyright = '2020, Alex J. Champandard'
+author = 'Alex J. Champandard'
+
+# -- General configuration ---------------------------------------------------
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'default'
+html_static_path = ['_static']
---
+++ src/index.rst
@@ -0,0 +1,15 @@
+The Science and Magic of Creativity
+===================================
+
+Table Of Content
+----------------
+
+.. toctree::
+    :maxdepth: 2
+
+
+Indices and Search
+------------------
+
+* :ref:`genindex`
+* :ref:`search`
